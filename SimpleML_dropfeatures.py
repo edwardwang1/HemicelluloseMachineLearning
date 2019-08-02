35f8872ed26e868e3a8f8c97a6419c866022e55f
rballachay <51099841+rballachay@users.noreply.github.com>
2019-08-02 10:08:26 -0700

    Update SimpleML_dropfeatures.py

--- SimpleML_dropfeatures.py
+++ SimpleML_dropfeatures.py
@@ -21,72 +21,70 @@ from keras.regularizers import l1
 from keras.regularizers import l1_l2
 
 start = time.time()
-
-data_start = pd.read_csv("2048data.csv")
-
-# data = wd.deleter(data)
-
-
+  
+def validate(X,Y,modelname,epoch):
+        kfold = KFold(n_splits=4, shuffle=True)
+        cvscores = []
+        trainingscores =[]
+        split=0
+        for train, test in kfold.split(X,Y):
+        	# Fit the model
+            modelname.fit(X[train], Y[train], epochs=epoch, batch_size=best_bs, verbose=0)
+            y_pred = modelname.predict(X[test])
+            y_train = modelname.predict(X[train] )
+            y_train = y_train.flatten()
+            y_pred = y_pred.flatten()
+            try:
+                training_error = metrics.mean_absolute_error(Y[train], y_train)
+                error = metrics.mean_absolute_error(Y[test], y_pred)
+                trainingscores.append(training_error)
+            except:
+                print("Input contains null values. Skipping Config.")
+                continue
+            cvscores.append(error)
+            split=split+1
+        print("Validation Score: %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+        print("Training Score: %.2f%% (+/- %.2f%%)" % (np.mean(trainingscores), np.std(trainingscores)))
+        return
+    
+    
 # Prepping Data
-data_start = data_start.sample(frac=1).reset_index(drop=True)
-XLabels = ['TotalT', 'Temp', 'LSR', 'CA', 'Size', 'IsoT', 'HeatT', 'Ramp', 'F_X', 'Ro', 'logRo', 'P','Acid','Acetyl','Wood','Yield']
-X = data_start[XLabels]
-# The data preparation function
-
+# data_start = data_start.sample(frac=.85).reset_index(drop=True)
+data_start = pd.read_csv("2048data.csv")
 XLabels = ['TotalT', 'Temp', 'LSR', 'CA', 'Size', 'IsoT', 'HeatT', 'Ramp', 'F_X', 'Ro', 'logRo', 'P','Acid','Acetyl','Wood','Yield']
 X_raw = data_start[XLabels]
-
-
-X,Y,data,XLabels=dp.prep(X_raw,True)
-
-best_lr = 0.01
-best_bs = 256
-best_dr = 0
-dropout=0.001
-initializer='lecun_uniform'
-
-def validate(X,Y,modelname,epoch):
-    kfold = KFold(n_splits=4, shuffle=True, random_state=1)
-    cvscores = []
-    trainingscores =[]
-    split=0
-    for train, test in kfold.split(X,Y):
-    	# Fit the model
-        modelname.fit(X[train], Y[train], epochs=epoch, batch_size=best_bs, verbose=0)
-        y_pred = modelname.predict(X[test])
-        y_train = modelname.predict(X[train])
-        y_train = y_train.flatten()
-        y_pred = y_pred.flatten()
-        try:
-            training_error = metrics.mean_absolute_error(Y[train], y_train)
-            error = metrics.mean_absolute_error(Y[test], y_pred)
-            trainingscores.append(training_error)
-        except:
-            print("Input contains null values. Skipping Config.")
-            continue
-        cvscores.append(error)
-        split=split+1
-    print("Validation Score: %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
-    print("Training Score: %.2f%% (+/- %.2f%%)" % (np.mean(trainingscores), np.std(trainingscores)))
-    return
-
-epochs = [1,10,50,100,500,1000,2500,5000,10000]
-
-
-for epoch in epochs:
+# The data preparation function
+for drop_this in XLabels:
+    
+    print("Dropped %s from the data" % drop_this)
+    
+    X,Y,data,XLabels=dp.prep(X_raw,True)
+    
+    index=XLabels.index(drop_this)
+    
+    X=np.delete(X,index,axis=1)
+    
+    best_lr = 0.01
+    best_bs = 256
+    best_dr = 0
+    dropout=0.001
+    initializer='lecun_uniform'
+    epoch = 100
+    
+     
+    
     try:
         model = Sequential()
-        model.add(Dense(units=96, activation='softsign', input_dim=39, kernel_initializer=initializer))
+        model.add(Dense(units=96, activation='softsign', input_dim=38, kernel_initializer=initializer))
         model.add(Dropout(dropout))
+        model.add(Dense(units=96, activation='softsign', kernel_initializer=initializer))
         model.add(Dense(units=48, activation='softsign', kernel_initializer=initializer))
         model.add(Dense(units=48, activation='softsign', kernel_initializer=initializer))
         model.add(Dense(units=1, activation='linear'))
     except:
         print("That isn't a valid initializer, stupid")
-        continue
     sgd = SGD(lr=best_lr)
     model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy'])
-    print('Number of Epochs:',epoch)
     validate(X,Y,model,epoch)
     end1 = time.time()
     duration = end1 - start
