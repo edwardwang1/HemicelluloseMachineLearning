b953e43142eebc884c9b431d34fedb54f2821bd0
Edward <ed1996@gmail.com>
2019-10-08 17:39:11 -0700

    New Folder for new tests

---
+++ FinalFilesNew/ComparingMLModelsMonomer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --time=0-03:00      # time (DD-HH:MM)
+#SBATCH --output=ComparingML-%N-%j.out  # %N for node name, %j for jobID
+
+module load python/3.6
+module load scipy-stack
+module load cuda cudnn 
+source $HOME/hemicellulose_project/bin/activate
+
+python ./ComparingMLModelsMonomer.py
